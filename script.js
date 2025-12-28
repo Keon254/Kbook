55bb51409852c6ffd579e3c7ccc2f8d8bda73138
Keon254 <blessmall212@gmail.com>
2025-12-28 13:10:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,28 @@ const postBtn = document.getElementById("postBtn");
 const postInput = document.getElementById("postInput");
 const feed = document.getElementById("feed");
 
+// Click button to post
 postBtn.addEventListener("click", createPost);
+
+// Press Enter to post
+postInput.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    createPost();
+  }
+});
 
 function createPost() {
   const text = postInput.value.trim();
   if (text === "") return;
+
+  const time = new Date().toLocaleString();
 
   const post = document.createElement("div");
   post.className = "post";
 
   post.innerHTML = `
     <h4>You</h4>
+    <small>${time}</small>
     <p>${text}</p>
     <div class="actions">
       <button class="like-btn">Like (<span>0</span>)</button>
